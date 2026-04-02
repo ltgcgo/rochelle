219c7c953e3c6fe066e7e3b820de35d85bab21c5
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-04-02 06:35:05 +0000

    Source code sync.

--- src/splicer/index.mjs
+++ src/splicer/index.mjs
@@ -1,17 +1,100 @@
-// Copyright 2024 (C) Lightingale Community
+// 2024-2026 © Lightingale Community
 // Licensed under GNU LGPL 3.0
 
 "use strict";
 
 // Asynchronously enqueue readable streams, and combine multiple readable streams together. State of the streams supplying to the queue does not matter.
-let StreamQueue = class StreamQueue {};
+const StreamQueue = class StreamQueue {
+	#controller;
+	#pullPromise;
+	#closedResolve;
+	closed = false;
+	closure;
+	cancelled;
+	readable;
+	constructor(underlyingSource = {}, queuingStrategy) {
+		let upThis = this;
+		let enqueueResolve, enqueueReject, cancelledResolve;
+		upThis.cancelled = new Promise((p) => {
+			cancelledResolve = p;
+		});
+		upThis.closure = new Promise((p) => {
+			upThis.#closedResolve = p;
+		})
+		upThis.readable = new ReadableStream({
+			"type": underlyingSource.type,
+			"autoAllocateChunkSize": underlyingSource.autoAllocateChunkSize,
+			"cancel": async (reason) => {
+				enqueueReject(reason);
+				cancelledResolve(reason);
+				upThis.#closedResolve();
+				upThis.closed = true;
+				underlyingSource.cancelled?.call(upThis, reason);
+			},
+			"start": async (controller) => {
+				upThis.#controller = controller;
+				console.debug(`Stream start.`);
+				underlyingSource.start?.call(upThis, new Proxy(controller, {
+					"get": (target, key) => {
+						switch (key) {
+							case "enqueue":
+							case "error":
+							case "close": {
+								return upThis[key];
+								break;
+							};
+							default: {
+								return target[key];
+							};
+						};
+					}
+				}));
+				console.debug(`Source start called.`);
+				upThis.#pullPromise = new Promise((p, r) => {
+					enqueueResolve = p;
+					enqueueReject = r;
+				});
+			},
+			"pull": async (controller) => {
+				enqueueResolve();
+				upThis.#pullPromise = new Promise((p, r) => {
+					enqueueResolve = p;
+					enqueueReject = r;
+				});
+				console.debug(`Stream pull.`);
+			}
+		}, queuingStrategy);
+	};
+	enqueue(chunk) {
+		this.#controller.enqueue(chunk);
+		return this.#pullPromise;
+	};
+	close() {
+		let upThis = this;
+		upThis.#controller.close();
+		upThis.#closedResolve();
+		upThis.closed = true;
+	};
+	error(err) {
+		let upThis = this;
+		upThis.#controller.error(err);
+		upThis.#closedResolve();
+		upThis.closed = true;
+	};
+	pipeFrom(source) {
+		(async () => {
+			for await (let chunk of source) {
+				await this.enqueue(chunk);
+			};
+		})();
+	};
+};
 
 // Split one readable stream into multiple. State of the subsequent streams do not affect the source stream. Reading from any of the subsequent streams will relieve the backpressure of the source stream.
-let StreamServe = class StreamServe {};
+const StreamServe = class StreamServe {};
 
-// Normalize chunks of a byte stream to a specific size.
-// Moved from WingBlade.
-let ChokerStream = class ChokerStream {};
+// Normalize chunks of a byte stream to a specific size. Moved from WingBlade.
+const ChokerStream = class ChokerStream {};
 
 export {
 	StreamQueue,
